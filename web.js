01522509612eb623b5b0f1d948f738775b95f3f3
Evan Siegel <siegel.evan@gmail.com>
2014-02-01 15:12:18 -0500

    Add LESS and EJS

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require("express"),
+    path = require("path"),
     logfmt = require("logfmt"),
     MailChimpAPI = require("mailchimp").MailChimpAPI,
     mc_api,
@@ -8,9 +9,15 @@ var express = require("express"),
 mc_api = new MailChimpAPI(process.env.MAILCHIMP_API_KEY, { version: 2.0 });
 
 app.use(logfmt.requestLogger());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.render('index');
 });
 
 port = Number(process.env.PORT || 5000);
