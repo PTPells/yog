cf6d130b59de3f150a1f58cb662bf971bbbbc0fa
Evan Siegel <siegel.evan@gmail.com>
2014-02-05 23:34:29 -0500

    Change Email Response Handling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,23 +4,30 @@ function EmailCapture (id) {
     $button = $($root.find('button'));
     $message = $($root.find('.message'));
 
-    $button.click(function () {
-        var data = { email: $input.val() };
-        $.post('/email', data, function(res, textStatus, xhr) {
-            showMessage(res);
-        });
+    $button.click(sendEmail);
+    $input.keypress(function (event) {
+        /* Act on the event */
     });
 
-    function showMessage (res) {
-        var msg;
-        if (res.success) {
-            console.log(res.data);
-            msg = "Please make sure to click the link in the confirmation email.";
-        } else {
-            console.log(res.message);
-            msg = res.message;
+    function sendEmail () {
+        var data = { email: $input.val() };
+        if (!data.email) {
+            showMessage("You didn't enter an email address.", 'failure');
+            return false;
         }
-        $message.text(msg).show();
+        $.post('/email', data, function(res, textStatus, xhr) {
+            if (res.success) {
+                console.log(res.data);
+                showMessage("Success! Please make sure to click the link in the confirmation email.", 'success');
+            } else {
+                console.log(res.message);
+                showMessage(res.message, 'failure');
+            }
+        });
+    }
+
+    function showMessage (msg, type) {
+        $message.addClass(type).text(msg).show();
     }
 }
 
